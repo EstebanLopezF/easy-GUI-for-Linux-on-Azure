8386e17dd70f49784174e1e362e18d454966da39
elopezf-cenfo <elopezf_@outlook.com>
2019-02-15 22:32:06 -0600

    add support for redhat 7.[1-6]

--- easyGUI.sh
+++ easyGUI.sh
@@ -15,45 +15,47 @@ logpath=/var/log/easyGUI.log
 
 case $distro in
 
-centos)
-    if [ version == 7.[1-4] ]; then	
-		yum groupinstall 'Server with GUI' -y >> $logpath 2>&1 ; 
-		firewall-cmd --permanent --zone=public --add-port=3389/tcp >> $logpath 2>&1 ; 
-		firewall-cmd --reload >> $logpath 2>&1 ; 
-		rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm >> $logpath 2>&1 ; 
-		yum install xrdp tigervnc-server xterm -y --skip-broken >> $logpath 2>&1 ; 
-		systemctl start xrdp >> $logpath 2>&1 ; 
-		systemctl start xrdp-sesman >> $logpath 2>&1 ; 
-		systemctl enable xrdp >> $logpath 2>&1 ; 
-		systemctl enable xrdp-sesman >> $logpath 2>&1 ; 
-		systemctl set-default graphical.target >> $logpath 2>&1 ; 
-		systemctl isolate graphical.target >> $logpath 2>&1 ;          	
+  redhat)
+  centos)
+        if [ version == 7.[1-6] ]; then	
+	    yum groupinstall 'Server with GUI' -y >> $logpath 2>&1 ; 
+	    firewall-cmd --permanent --zone=public --add-port=3389/tcp >> $logpath 2>&1 ; 
+	    firewall-cmd --reload >> $logpath 2>&1 ; 
+	    rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm >> $logpath 2>&1 ; 
+	    yum install xrdp tigervnc-server xterm -y >> $logpath 2>&1 ; 
+	    systemctl start xrdp >> $logpath 2>&1 ; 
+	    systemctl start xrdp-sesman >> $logpath 2>&1 ; 
+	    systemctl enable xrdp >> $logpath 2>&1 ; 
+	    systemctl enable xrdp-sesman >> $logpath 2>&1 ; 
+	    systemctl set-default graphical.target >> $logpath 2>&1 ; 
+	    systemctl isolate graphical.target >> $logpath 2>&1 ;          	
 	fi;;
-Ubuntu)
+
+  Ubuntu)
 	if [ version == 17.* -o version == 18.04* ]; then                                                               	
-		apt-get update -y >> $logpath 2>&1 ;                                        
-		apt-get install xfce4 -y >> $logpath 2>&1 ;
-		apt-get install xrdp -y >> $logpath 2>&1 ;
-		apt-get install xserver-xorg-dev -y >> $logpath 2>&1 ;
-		echo xfce4-session >~/.xsession >> $logpath 2>&1 ;                            
-		/etc/init.d/xrdp start >> $logpath 2>&1 ;
-		systemctl set-default graphical.target >> $logpath 2>&1 ;
-		systemctl isolate graphical.target >> $logpath 2>&1 ;
-		sed 's/allowed_users=console/allowed_users=anybody/g' /etc/X11/Xwrapper.config -i >> $logpath 2>&1  
-		systemctl enable xrdp-sesman.service >> $logpath 2>&1 ;
-		systemctl restart xrdp-sesman.service >> $logpath 2>&1 ;
-		systemctl enable xrdp >> $logpath 2>&1 ;
-		systemctl restart xrdp >> $logpath 2>&1 ;
-	elif [ version == 16.* ]
-		then		                                                                            		
-			apt-get update -y >> $logpath 2>&1 ;
-			sudo apt-get install xfce4 -y >> $logpath 2>&1 ;
-			sudo apt-get install xrdp -y >> $logpath 2>&1 ;
-			echo xfce4-session >~/.xsession >> $logpath 2>&1 ;
-			/etc/init.d/xrdp start >> $logpath 2>&1 ;
-			systemctl isolate graphical.target >> $logpath 2>&1 ;                                    
+	    apt-get update -y >> $logpath 2>&1 ;                                        
+	    apt-get install xfce4 -y >> $logpath 2>&1 ;
+	    apt-get install xrdp -y >> $logpath 2>&1 ;
+	    apt-get install xserver-xorg-dev -y >> $logpath 2>&1 ;
+	    echo xfce4-session >~/.xsession >> $logpath 2>&1 ;                            
+	    /etc/init.d/xrdp start >> $logpath 2>&1 ;
+	    systemctl set-default graphical.target >> $logpath 2>&1 ;
+	    systemctl isolate graphical.target >> $logpath 2>&1 ;
+	    sed 's/allowed_users=console/allowed_users=anybody/g' /etc/X11/Xwrapper.config -i >> $logpath 2>&1  
+	    systemctl enable xrdp-sesman.service >> $logpath 2>&1 ;
+	    systemctl restart xrdp-sesman.service >> $logpath 2>&1 ;
+	    systemctl enable xrdp >> $logpath 2>&1 ;
+	    systemctl restart xrdp >> $logpath 2>&1 ;
+
+	elif [ version == 16.* ]; then		                                                                            		
+	    apt-get update -y >> $logpath 2>&1 ;
+	    sudo apt-get install xfce4 -y >> $logpath 2>&1 ;
+	    sudo apt-get install xrdp -y >> $logpath 2>&1 ;
+	    echo xfce4-session >~/.xsession >> $logpath 2>&1 ;
+	    /etc/init.d/xrdp start >> $logpath 2>&1 ;
+	    systemctl isolate graphical.target >> $logpath 2>&1 ;                                    
 	fi;;
-SuSE)
+  SuSE)
 		if [ version == 12-SP[1-3] ]; then                                                          
 			zypper -n install -t pattern gnome-basic >> $logpath 2>&1 ;
 			zypper -n install xrdp >> $logpath 2>&1 ;
